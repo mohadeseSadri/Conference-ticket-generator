7a917fc3aaba4da7f30896a78cd1e97b80b15b7b
Mohadese <Mohadesehsadr788@gmail.com>
2025-02-05 16:37:58 +0330

    Project start

---
+++ Conference ticket generator/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header";
+
+
+function App() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
