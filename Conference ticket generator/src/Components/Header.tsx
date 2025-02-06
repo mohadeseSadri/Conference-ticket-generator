527bfd3a455c729bc923ff7ce87a2de6c8c17077
Mohadese <Mohadesehsadr788@gmail.com>
2025-02-06 11:38:28 +0330

    Updating header

--- Conference ticket generator/src/Components/Header.tsx
+++ Conference ticket generator/src/Components/Header.tsx
@@ -2,17 +2,23 @@ import logo from "../assets/logo-full.png";
 function Header() {
   return (
     <>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center mt-5">
         <div className="flex gap-4">
           <img src={logo} alt="logo w-[200px] h-[100px]" />
           <h5 className="text-xl font-bold pt-2">Coding conf</h5>
         </div>
       </div>
 
-      <div className="flex items-center justify-center mt-10">
+      <div className="flex items-center justify-center mt-7">
         <h1 className="text-5xl font-black text-center">
           Your Journey to Coding Conf  2025 starts Here!
         </h1>
+      </div>
+
+      <div className="flex items-center justify-center mt-10">
+        <p className="text-center">
+          Secure your spot at next year's biggest coding conference
+        </p>
       </div>
     </>
   );
