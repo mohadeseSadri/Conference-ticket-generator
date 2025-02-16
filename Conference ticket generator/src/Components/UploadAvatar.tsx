99065d2a7095dd505566a5f45549ddb62c7ce92e
Mohadese <Mohadesehsadr788@gmail.com>
2025-02-16 16:42:50 +0330

    Handeling Errors Upload avatar

--- Conference ticket generator/src/Components/UploadAvatar.tsx
+++ Conference ticket generator/src/Components/UploadAvatar.tsx
@@ -5,6 +5,7 @@ import Info from "../assets/Images/icon-info.svg";
 function UploadAvatar() {
   const [image, setImage] = useState<string | null>(null);
   const [draggedFiles, setDraggedFiles] = useState<File[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   const handleImageUplod = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -21,6 +22,10 @@ function UploadAvatar() {
     event.preventDefault();
     const files = Array.from(event.dataTransfer.files);
     setDraggedFiles((prevFiles) => [...prevFiles, ...files]);
+    if (files.length > 0) {
+      const file = files[0];
+      validateFile(file);
+    }
   };
 
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
@@ -29,6 +34,22 @@ function UploadAvatar() {
 
   const clearFiles = () => setDraggedFiles([]);
 
+  const validateFile = (file: {
+    size: number; type: string 
+}) => {
+    setError("");
+    const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+    if (!allowedTypes.includes(file.type)) {
+      setError("Only JPEG, PNG and GIF files are allowed.");
+      return;
+    }
+
+    const maxSize = 2 * 1024 * 1024; 
+    if (file.size > maxSize) {
+      setError("File size must be less than 2MB.");
+      return;
+    }
+  };
   return (
     <>
       <div className="display:inline-block; position: relative">
@@ -47,7 +68,7 @@ function UploadAvatar() {
             <div className="flex items-center justify-center">
               <div className="text-center">
                 {image ? (
-                  <img src={image} alt="Uploade" className="preview-image" />
+                  <img src={image} alt="Uploade" />
                 ) : (
                   <>
                     <p className="text-xl font-medium text-gray-700">
@@ -55,31 +76,35 @@ function UploadAvatar() {
                     </p>
                   </>
                 )}
+                {error ? (
+                  ""
+                ) : (
+                  <>
+                    <div className="p-4">
+                      <div className="text-center cursor-pointer transition-colors">
+                        {draggedFiles.length > 0 && (
+                          <button
+                            type="button"
+                            className="mt-4 px-4 py-2 text-white rounded"
+                            onClick={clearFiles}
+                          >
+                            Clear Files
+                          </button>
+                        )}
+                      </div>
 
-                <div className="p-4">
-                  <div className="text-center cursor-pointer transition-colors">
-                    {draggedFiles.length > 0 && (
-                      <button
-                        type="button"
-                        className="mt-4 px-4 py-2 text-white rounded"
-                        onClick={clearFiles}
-                      >
-                        Clear Files
-                      </button>
-                    )}
-                  </div>
-
-                  {draggedFiles.length > 0 && (
-                    <div className="grid grid-cols-1 gap-4 mt-6">
-                      {draggedFiles.map((file, index) => (
-                        <div key={index} className="p-4 rounded shadow">
-                          <p>{file.name}</p>
+                      {draggedFiles.length > 0 && (
+                        <div className="grid grid-cols-1 gap-4 mt-6">
+                          {draggedFiles.map((file, index) => (
+                            <div key={index} className="p-4 rounded shadow">
+                              <p>{file.name}</p>
+                            </div>
+                          ))}
                         </div>
-                      ))}
+                      )}
                     </div>
-                  )}
-                </div>
-
+                  </>
+                )}
                 <input
                   type="file"
                   accept="image/*"
@@ -90,12 +115,18 @@ function UploadAvatar() {
             </div>
           </div>
         </div>
-
-        <div className="flex items-center justify-center mt-2">
-          <img src={Info} alt="Info" className="pr-2" />
-          <p className="text-[13px] text-stone-300">Upload your photo (JPG or PNG, max size 500kB)</p>
-        </div>
-
+        {error ? (
+          <p className="text-red-500 flex items-center justify-center mt-2">
+            {error}
+          </p>
+        ) : (
+          <div className="flex items-center justify-center mt-2">
+            <img src={Info} alt="Info" className="pr-2" />
+            <p className="text-[13px] text-stone-300">
+              Upload your photo (JPG or PNG, max size 2MB)
+            </p>
+          </div>
+        )}
       </div>
     </>
   );
