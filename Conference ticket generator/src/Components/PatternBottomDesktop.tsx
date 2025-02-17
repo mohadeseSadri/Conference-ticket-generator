417fb417db498e4612d18d432e6d0127c79a5194
Mohadese <Mohadesehsadr788@gmail.com>
2025-02-17 16:45:52 +0330

    Styling the Pattern

--- Conference ticket generator/src/Components/PatternBottomDesktop.tsx
+++ Conference ticket generator/src/Components/PatternBottomDesktop.tsx
@@ -2,7 +2,7 @@ import PatternBottom from "../assets/Images/pattern-squiggly-line-bottom-desktop
 function PatternBottomDesktop() {
   return (
     <div>
-        <img src={PatternBottom} alt="" className="absolute bottom-0 left-0" />
+        <img src={PatternBottom} alt="" className="absolute bottom-0 left-0 z-[-1]" />
     </div>
   )
 }
